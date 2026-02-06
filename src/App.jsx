6964473e151827715a468ea4197bf07e6b2bce5e
Vishwakarma28 <Amit9301736578@gmail.com>
2026-02-06 22:26:44 +0530

    changeApiurl

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     console.log("Sending data:", data);
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/predict', {
+      const response = await fetch('heartdiseasepredictaibackend.vercel.app/predict', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
